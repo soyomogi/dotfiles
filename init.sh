4b796c1f5af41463cccacd79642d9cb9b6b5ad1a
soyomogi <soyomogi@gmail.com>
2025-09-22 02:57:38 +0900

    一旦snacksはそのまま

--- init.sh
+++ init.sh
@@ -14,11 +14,15 @@ rm $HOME/.vimrc
 ln -s $GITDIR/.vimrc $HOME/.vimrc
 mkdir -p $HOME/.vim/colors
 if [ ! -e $HOME/.vim/colors/molokai.vim ]; then
-  curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim > $HOME/.vim/colors/molokai.vim
+  curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim >$HOME/.vim/colors/molokai.vim
 fi
 if [ ! -e $HOME/.vim/colors/fairyfloss.vim ]; then
-  curl https://raw.githubusercontent.com/tssm/fairyfloss.vim/master/colors/fairyfloss.vim > $HOME/.vim/colors/fairyfloss.vim
+  curl https://raw.githubusercontent.com/tssm/fairyfloss.vim/master/colors/fairyfloss.vim >$HOME/.vim/colors/fairyfloss.vim
 fi
+
+# neovim
+rm $HOME/.config/nvim
+ln -s $GITDIR/.config/nvim $HOME/nvim
 
 # other
 rm $HOME/.screenrc
