2adee55bcf9c236d9e82bb099ec6846b355f844b
soyomogi <soyomogi@gmail.com>
2025-09-22 20:06:55 +0900

    fix snack terminal

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,10 +2,31 @@ return {
   "folke/snacks.nvim",
   lazy = false,
   priority = 1000,
+  init = function()
+    vim.api.nvim_set_hl(0, "SnacksTerminal", { bg = "#ffe5f3", fg = "#3b2f4a" })
+    vim.api.nvim_set_hl(0, "SnacksTerminalBorder", { bg = "#ffe5f3", fg = "#ffe5f3" })
+  end,
   opts = {
     picker = {
       source = {
         explorer = { hidden = true },
+      },
+    },
+    terminal = {
+      win = {
+        style = "split",
+        position = "bottom",
+        height = 12,
+        wo = {
+          winhighlight = table.concat({
+            "Normal:SnacksTerminal",
+            "NormalNC:SnacksTerminal",
+            "SignColumn:SnacksTerminal",
+            "EndOfBuffer:SnacksTerminal",
+          }, ","),
+          number = false,
+          relativenumber = false,
+        },
       },
     },
   },
