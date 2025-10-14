890b2dae4e37d092d560c2d7f4400185d8ef1d26
soyomogi <soyomogi@gmail.com>
2025-10-15 01:05:02 +0900

    rm vim config off

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
   callback = function()
     if vim.bo.buftype == "terminal" then
-      -- SnacksのTerminalと同じ見た目を適用
+      -- Snacks Terminal Color
       vim.api.nvim_set_hl(0, "SnacksTerminal", { bg = "#ffe5f3", fg = "#3b2f4a" })
       vim.api.nvim_set_hl(0, "SnacksTerminalBorder", { bg = "#ffe5f3", fg = "#ffe5f3" })
       vim.wo.winhighlight = table.concat({
@@ -11,10 +11,8 @@ vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
         "EndOfBuffer:SnacksTerminal",
         "FloatBorder:SnacksTerminalBorder",
       }, ",")
-      vim.wo.number = false
-      vim.wo.relativenumber = false
     else
-      -- 他のバッファに戻ったらもとの色へ
+      -- restore color
       vim.wo.winhighlight = ""
     end
   end,
